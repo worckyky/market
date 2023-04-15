af5b6953273e34710df65cc8ffb82cf2a90d6ac9
Anatoly Mitroshin <anatoly.mitroshin@setinbox.com>
2023-04-15 21:34:31 +0300

    -dist 1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/market/'
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/market/'
+      : '/'
 })
