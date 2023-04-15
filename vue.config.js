cc909c29951e38bc0b7a46ea9b96b0e542239353
Anatoly Mitroshin <anatoly.mitroshin@setinbox.com>
2023-04-15 22:05:41 +0300

    new config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/market/'
-      : '/',
+  publicPath: '/',
   pwa: {
     workboxOptions: {
       exclude: [/_redirects/]
